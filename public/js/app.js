ed98c2052287c885e0431c10e70ef317198df9e4
unknown <metoantul@gmail.com>
2020-10-28 15:26:02 +0100

    Remove uncessert console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,3 @@
-console.log('CLient side JAVASCRIPT')
-
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
